95b54ec8ffffb794f77009d8c1ba5635883420d9
andryandroid <andjos27@gmail.com>
2020-07-14 20:17:02 -0400

    actualizacion signup

--- cliente/src/Vistas/Signup.js
+++ cliente/src/Vistas/Signup.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Main from '../Componentes/Main';
 import imagenSignup from '../imagenes/signup.png';
+import { useState } from 'react';
 
 export default function Signup() {
+
+    const usuario = {
+        email: '',
+        name: '',
+        username: '',
+        bio: '',
+        password: ''
+    }
+
+    function handleInputChange(e){
+        usuario[e.target.name] = e.target.value;
+        console.log(usuario);
+    }
+
     return(
         <Main center={true}>
             <div className="Signup">
@@ -13,11 +28,52 @@ export default function Signup() {
                         Registrate para que veas el clon de Instagram
                     </p>
                     <form>
-                        <input type="email" name="email" placeholder="Email" className="Form__field" required></input>
-                        <input type="text" name="name" placeholder="Nombre y Apellido" minLength="3" maxLength="100" className="Form__field" required></input>
-                        <input type="text" name="username" placeholder="Nombre de Usuario" minLength="3" maxLength="30" className="Form__field" required></input>
-                        <input type="text" name="bio" placeholder="Cuentanos de ti..." minLength="3" maxLength="150" className="Form__field" required></input>
-                        <input type="password" name="password" placeholder="Contraseña" className="Form__field" required></input>
+                        <input
+                            type="email" 
+                            name="email" 
+                            placeholder="Email" 
+                            className="Form__field"
+                            onChange={handleInputChange} 
+                            required>
+                        </input>
+                        <input 
+                            type="text" 
+                            name="name" 
+                            placeholder="Nombre y Apellido" 
+                            minLength="3" 
+                            maxLength="100" 
+                            className="Form__field"
+                            onChange={handleInputChange} 
+                            required>
+                        </input>
+                        <input type="text" 
+                            name="username" 
+                            placeholder="Nombre de Usuario" 
+                            minLength="3" 
+                            maxLength="30" 
+                            className="Form__field"
+                            onChange={handleInputChange} 
+                            required>
+                        </input>
+                        <input 
+                            type="text" 
+                            name="bio" 
+                            placeholder="Cuentanos de ti..." 
+                            minLength="3" 
+                            maxLength="150" 
+                            className="Form__field"
+                            onChange={handleInputChange} 
+                            required>
+                        </input>
+                        <input 
+                            type="password" 
+                            name="password" 
+                            placeholder="Contraseña" 
+                            className="Form__field"
+                            onChange={handleInputChange} 
+                            required> 
+                        </input>
+                        
                         <button className="Form__submit" type="submit">Sign Up</button>
                         <p className="FormContainer__info">
                             ¿Ya tienes cuenta? <a href="#">Login</a>
