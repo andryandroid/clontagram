79f9dde3c0ae3a9c0d11e5ea282118e8874ecada
andryandroid <andjos27@gmail.com>
2020-07-23 22:38:18 -0400

    consultar si existe usuario logeado

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,14 +1,35 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Axios from 'axios';
 
-import {setToken, deleteToken} from './Helpers/auth-helpers';
+import {setToken, deleteToken, getToken, initAxiosInterceptors} from './Helpers/auth-helpers';
 import Nav from './Componentes/Nav';
 
 import Signup from './Vistas/Signup';
 import Login from './Vistas/Login';
 
+initAxiosInterceptors();
+
 export default function App() {
   const [usuario, setUsuario] = useState(null); 
+  const [cargandoUsuario, setCargandoUsuario] = useState(true);
+
+  useEffect(() => {
+    async function cargarUsuario(){
+      if(!getToken()){
+        setCargandoUsuario(false);
+        return;
+      }
+      
+      try{
+        const {data: usuario} = await Axios.get('/api/usuarios/whoami');
+        setUsuario(usuario);
+        setCargandoUsuario(false);
+      }catch(error){
+        console.log(error);
+      }
+    }
+    cargarUsuario();
+  }, []);
 
   async function login(email, password){
     const { data } = await Axios.post('/api/usuarios/login', {
