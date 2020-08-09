2ab83ccb4b52bb07fd0fc90f6f5086807214da4c
andryandroid <andjos27@gmail.com>
2020-08-09 19:47:42 -0400

    adding post image

--- cliente/src/Vistas/Upload.js
+++ cliente/src/Vistas/Upload.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import { faTimesCircle} from '@fortawesome/free-regular-svg-icons';
+import { faUpload} from '@fortawesome/free-solid-svg-icons';
 import Main from '../Componentes/Main';
+import Loading from '../Componentes/Loading';
+import Axios from 'axios';
 
 export default function Upload(){
     return (
         <Main center>
-            <h1>Sube tu pic guapo ;)</h1>
+            <div className = "Upload" >
+                <form>
+                    <div className = "Upload__image-section"/>
+                    <textarea 
+                        name = "caption" 
+                        className = "Upload__caption" 
+                        required 
+                        maxLength = "180"
+                        placeholder = "Caption de tu post."
+                    />
+                    <button className = "Upload__submit" type = "submit" > Post</button>
+                </form>
+            </div>
         </Main>
     )
 }
