edce0e115de70a0c9c7e124414d0a2c440309846
andryandroid <andjos27@gmail.com>
2020-08-16 19:16:57 -0400

    adding upload photo

--- cliente/src/Vistas/Upload.js
+++ cliente/src/Vistas/Upload.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { faTimesCircle} from '@fortawesome/free-regular-svg-icons';
-import { faUpload} from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimesCircle } from '@fortawesome/free-regular-svg-icons';
+import { faUpload } from '@fortawesome/free-solid-svg-icons';
 import Main from '../Componentes/Main';
 import Loading from '../Componentes/Loading';
 import Axios from 'axios';
 
 export default function Upload(){
+    const [imagenUrl, setImagenUrl] = useState('');
+    const [subiendoImagen, setSubiendoImagen] = useState(true);
+
     return (
         <Main center>
             <div className = "Upload" >
                 <form>
-                    <div className = "Upload__image-section"/>
+                    <div className = "Upload__image-section">
+                        <SeccionSubirImagen
+                            imagenUrl={imagenUrl}
+                            subiendoImagen={subiendoImagen}
+                        />
+                    </div>
                     <textarea 
                         name = "caption" 
                         className = "Upload__caption" 
@@ -24,4 +32,20 @@ export default function Upload(){
             </div>
         </Main>
     )
+}
+
+function SeccionSubirImagen({subiendoImagen, imagenUrl}){
+    if (subiendoImagen){
+        return <Loading />
+    } else if (imagenUrl){
+        return <img src={imagenUrl} alt=""/>
+    } else{
+        return(
+            <label className="Upload__image-label">
+                <FontAwesomeIcon icon={faUpload}></FontAwesomeIcon>
+                <span>Publica una foto</span>
+                <input type="file" className="hidden" name="imagen"></input>
+            </label>
+        )
+    }
 }
